76f68c46084f6d513fba3507c2de94cb37a5920b
郑程鹏 <zhengchengpengisme@gmail.com>
2017-07-11 17:12:18 +0800

    修改：提现布局

--- interhis/app/src/main/java/com/witnsoft/interhis/setting/myincome/WithdrawCrashFragment.java
+++ interhis/app/src/main/java/com/witnsoft/interhis/setting/myincome/WithdrawCrashFragment.java
@@ -2,6 +2,9 @@ package com.witnsoft.interhis.setting.myincome;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -65,6 +68,23 @@ public class WithdrawCrashFragment extends ChildBaseFragment {
     }
 
     private void init() {
+        etCount.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_CLASS_NUMBER);
+        etCount.setFilters(new InputFilter[]{new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                if (source.equals(".") && dest.toString().length() == 0) {
+                    return "0.";
+                }
+                if (dest.toString().contains(".")) {
+                    int index = dest.toString().indexOf(".");
+                    int length = dest.toString().substring(index).length();
+                    if (length == 3) {
+                        return "";
+                    }
+                }
+                return null;
+            }
+        }});
     }
 
 }
