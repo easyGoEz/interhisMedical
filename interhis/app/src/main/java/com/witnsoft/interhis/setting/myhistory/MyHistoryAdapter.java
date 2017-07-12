dfca437b60db300d90d6e86931b493210dcb7226
郑程鹏 <zhengchengpengisme@gmail.com>
2017-07-12 15:53:47 +0800

    增加：我的问诊记录列表接口

--- interhis/app/src/main/java/com/witnsoft/interhis/setting/myhistory/MyHistoryAdapter.java
+++ interhis/app/src/main/java/com/witnsoft/interhis/setting/myhistory/MyHistoryAdapter.java
@@ -1,7 +1,6 @@
 package com.witnsoft.interhis.setting.myhistory;
 
 import android.content.Context;
-import android.text.TextUtils;
 
 import com.witnsoft.interhis.R;
 import com.witnsoft.interhis.utils.ComRecyclerAdapter;
@@ -14,23 +13,28 @@ import java.util.Map;
  * Created by zhengchengpeng on 2017/6/14.
  */
 
-public class MyHistoryAdapter extends ComRecyclerAdapter<String> {
+public class MyHistoryAdapter extends ComRecyclerAdapter<Map<String, String>> {
 
     private Context context;
 
-    public MyHistoryAdapter(Context context, List<String> list, int layoutId) {
+    public MyHistoryAdapter(Context context, List<Map<String, String>> list, int layoutId) {
         super(context, list, layoutId);
         this.context = context;
         this.list = list;
     }
 
     @Override
-    public void convert(ComRecyclerViewHolder comRecyclerViewHolder, String item) {
-        if (!TextUtils.isEmpty(item)) {
-            comRecyclerViewHolder.setText(R.id.fragment_doctor_recycleView_item_name, item);
-        } else {
-            comRecyclerViewHolder.setText(R.id.fragment_doctor_recycleView_item_name, "");
-        }
+    public void convert(ComRecyclerViewHolder comRecyclerViewHolder, Map<String, String> item) {
+        // 姓名
+        comRecyclerViewHolder.setText(R.id.fragment_doctor_recycleView_item_name, item.get("PATNAME"));
+        // 性别
+        comRecyclerViewHolder.setText(R.id.fragment_doctor_recycleView_item_sex, item.get("PATSEXNAME"));
+        // 年龄
+        comRecyclerViewHolder.setText(R.id.fragment_doctor_recycleView_item_age, item.get("PATNL"));
+        // 症状
+        comRecyclerViewHolder.setText(R.id.fragment_doctor_recycleView_item_content, item.get("JBMS"));
+        // 头像
+        comRecyclerViewHolder.setImageUrl(context, R.id.fragment_doctor_recycleView_item_image, item.get("PHOTOURL"), R.drawable.touxiang);
     }
 
     @Override
--- interhis/app/src/main/java/com/witnsoft/interhis/setting/myhistory/MyHistoryFragment.java
+++ interhis/app/src/main/java/com/witnsoft/interhis/setting/myhistory/MyHistoryFragment.java
@@ -1,5 +1,6 @@
 package com.witnsoft.interhis.setting.myhistory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -13,8 +14,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.witnsoft.interhis.R;
+import com.witnsoft.interhis.mainpage.LoginActivity;
 import com.witnsoft.interhis.setting.ChildBaseFragment;
 import com.witnsoft.interhis.utils.ComRecyclerAdapter;
+import com.witnsoft.libinterhis.utils.ThriftPreUtils;
+import com.witnsoft.libnet.model.DataModel;
+import com.witnsoft.libnet.model.OTRequest;
+import com.witnsoft.libnet.net.CallBack;
+import com.witnsoft.libnet.net.NetTool;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
@@ -22,6 +29,7 @@ import org.xutils.x;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zhengchengpeng on 2017/6/13.
@@ -29,9 +37,17 @@ import java.util.List;
 
 @ContentView(R.layout.fragment_my_history)
 public class MyHistoryFragment extends ChildBaseFragment {
-    View rootView;
+    private View rootView;
     private MyHistoryAdapter adapter;
-    private List<String> list = new ArrayList<>();
+    private List<Map<String, String>> historyList = new ArrayList<>();
+    private String docId;
+    private int pageNo = 1;
+    private static final int PAGE_COUNT = 10;
+
+    private final class ErrCode {
+        private static final String ErrCode_200 = "200";
+        private static final String ErrCode_504 = "504";
+    }
 
     // 总计接诊量
     @ViewInject(R.id.tv_visit_count_all)
@@ -64,27 +80,74 @@ public class MyHistoryFragment extends ChildBaseFragment {
     }
 
     private void init() {
+        docId = ThriftPreUtils.getDocId(getActivity());
         tvVisitCountAll.setText("33");
         tvVisitCountMonth.setText("22");
         tvVisitCountDaily.setText("11");
-        callList();
-
+        callSavePersonalIntroduction(true);
     }
 
-    private void callList() {
-        for (int i = 0; i < 8; i++) {
-            list.add(String.valueOf(i));
-        }
-        try {
-            freshUi();
-        } catch (NullPointerException e) {
+    /**
+     * F27.APP.01.11 我的问诊记录查询
+     */
+    private List<Map<String, String>> respList = new ArrayList<>();
 
-        }
+    private void callSavePersonalIntroduction(boolean isProgress) {
+        OTRequest otRequest = new OTRequest(getActivity());
+        // DATA
+        DataModel data = new DataModel();
+        data.setParam("docid", docId);
+        data.setParam("rowsperpage", String.valueOf(PAGE_COUNT));
+        data.setParam("pageno", String.valueOf(pageNo));
+        data.setParam("ordercolumn", "paytime");
+        data.setParam("ordertype", "asc");
+        otRequest.setDATA(data);
+        // TN 接口辨别
+        otRequest.setTN("F27.APP.01.11");
+
+        NetTool.getInstance().startRequest(false, isProgress, getActivity(), null, otRequest, new CallBack<Map, String>() {
+            @Override
+            public void onSuccess(Map response, String resultCode) {
+                if (ErrCode.ErrCode_200.equals(resultCode)) {
+                    if (null != response) {
+                        respList = new ArrayList<>();
+                        respList = (List<Map<String, String>>) response.get("DATA");
+                        if (null != respList && 0 < respList.size()) {
+                            if (1 == pageNo) {
+                                // 如果是第一页，表示重新加载数据
+                                historyList.clear();
+                                for (int i = 0; i < respList.size(); i++) {
+                                    historyList.add(respList.get(i));
+                                }
+                            } else {
+                                // 不是第一页，表示分页加载
+                                for (int i = 0; i < respList.size(); i++) {
+                                    historyList.add(respList.get(i));
+                                }
+                            }
+                        }
+                        freshUi();
+                    }
+
+                } else if (ErrCode.ErrCode_504.equals(resultCode)) {
+                    // token失效
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                slRefresh.setRefreshing(false);
+                slRefresh.setEnabled(true);
+            }
+        });
     }
 
     private void freshUi() {
         if (null == adapter) {
-            adapter = new MyHistoryAdapter(getActivity().getBaseContext(), list, R.layout.item_ask_history);
+            adapter = new MyHistoryAdapter(getActivity().getBaseContext(), historyList, R.layout.item_ask_history);
             adapter.setFootViewId(R.layout.activity_load_footer);
             rvHistory.setLayoutManager(new LinearLayoutManager(getActivity().getBaseContext()));
             rvHistory.setHasFixedSize(true);
@@ -94,7 +157,13 @@ public class MyHistoryFragment extends ChildBaseFragment {
         adapter.setOnRecyclerViewBottomListener(new ComRecyclerAdapter.OnRecyclerViewBottomListener() {
             @Override
             public void OnBottom() {
-
+                if (respList != null && respList.size() == PAGE_COUNT) {
+                    slRefresh.setEnabled(false);
+                    slRefresh.setRefreshing(true);
+                    adapter.setCanNotReadBottom(true);
+                    pageNo++;
+                    callSavePersonalIntroduction(true);
+                }
             }
         });
         adapter.setOnItemClickListener(new ComRecyclerAdapter.OnItemClickListener() {
@@ -124,8 +193,9 @@ public class MyHistoryFragment extends ChildBaseFragment {
                         slRefresh.setEnabled(false);
                         slRefresh.setRefreshing(true);
                         adapter = null;
-                        list.clear();
-                        callList();
+                        historyList.clear();
+                        pageNo = 1;
+                        callSavePersonalIntroduction(false);
                     }
                 }, 600);
             }
